99f98d22d05447e867ef104bde6e299b2fee844d
Boris Minin <boris.minin@outlook.com>
2021-01-13 13:57:59 +0300

    Add upload and download requests to ImageController, everything is working

--- UploadDownloadFile_WebAPI_Sample/Controllers/ImageController.cs
+++ UploadDownloadFile_WebAPI_Sample/Controllers/ImageController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using UploadDownloadFile_WebAPI_Sample.Models;
 
 namespace UploadDownloadFile_WebAPI_Sample.Controllers
 {
@@ -11,6 +14,76 @@ namespace UploadDownloadFile_WebAPI_Sample.Controllers
     [ApiController]
     public class ImageController : ControllerBase
     {
-    }
+        public static IWebHostEnvironment _environment;
+        public ImageController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
 
+        /// <summary>
+        /// Uploading selected image on server
+        /// </summary>
+        /// <param name="file">file that was selected for upload</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        {
+            if (file != null)
+            {
+                // готовим структуру данных для операции
+                string root = _environment.WebRootPath;
+                string folder = "\\Upload\\";
+                string path = root + folder;
+                string fileName = file.FileName;
+
+                // готовим окружение для операции
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                // выполняем операцию
+                await using (FileStream FileStream = System.IO.File.Create(path + fileName))
+                {
+                    file.CopyTo(FileStream);
+
+                    try
+                    {
+                        FileStream.Flush();// Очищает буферы для этого потока и вызывает запись всех буферизованных данных в файл
+                        string result = fileName + DateTime.Now.ToLongDateString();
+
+                        return new StatusCodeResult(200);
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        return new StatusCodeResult(404);
+                    }
+                    catch (PathTooLongException)
+                    {
+                        return new StatusCodeResult(500);
+                    }
+                }
+            }
+            else
+                return new StatusCodeResult(204);
+        }
+
+
+        //https://localhost:44325/api/DownloadImage/FILENAME.TYPEFILE      
+        /// <summary>
+        /// Downloading selected image from server
+        /// </summary>
+        /// <param name="filename">filename for download</param>
+        /// <returns></returns>
+        [HttpGet("{filename}")]
+        public async Task<IActionResult> DownloadIm(string filename)
+        {
+            var path = Path.GetFullPath("./wwwroot/upload/" + filename);
+            MemoryStream memory = new MemoryStream(); //чтение вводимых данных из массива или запись выводимых данных в массив
+            using (FileStream FileStream = new FileStream(path, FileMode.Open))
+            {
+                await FileStream.CopyToAsync(memory);
+            }
+            memory.Position = 0; //Текующая позиция в потоке (очистка)
+            return File(memory, "image/jpg", Path.GetFileName(path));
+        }
+    }
 }
--- UploadDownloadFile_WebAPI_Sample/Controllers/UploadImageController.cs
+++ UploadDownloadFile_WebAPI_Sample/Controllers/UploadImageController.cs
@@ -14,32 +14,39 @@ namespace UploadDownloadFile_WebAPI_Sample.Controllers
     [ApiController]
     public class UploadImageController
     {
-        public static IWebHostEnvironment _environment;
+            public static IWebHostEnvironment _environment;
         public UploadImageController(IWebHostEnvironment environment)
         {
             _environment = environment;
         }
 
-        //https://localhost:44325/api/UploadImage
-        /// <summary>
-        /// Chose image from directory and upload it on server
-        /// </summary>
-        /// <param name="objFile">file that was selected for upload</param>
-        /// <returns></returns>
-        [HttpPost]
-        public async Task<string> ImageUpload([FromForm] UploadImage_Model objFile)
-        {
-            if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
-                Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+        ////https://localhost:44325/api/UploadImage
+        ///// <summary>
+        ///// Choose image from directory and upload it on server
+        ///// </summary>
+        ///// <param name="objFile">file that was selected for upload</param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public async Task<string> ImageUpload([FromForm] UploadImage_Model objFile)
+        //{
+        //    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+        //        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
 
-            await using (FileStream FileStream = System.IO.File.Create
-                (_environment.WebRootPath + "\\Upload\\" + objFile.File.FileName))
-            {
-                objFile.File.CopyTo(FileStream);
-                FileStream.Flush();// Очищает буферы для этого потока и вызывает запись всех буферизованных данных в файл
-                return "added file " + objFile.File.FileName;
-            }
-        }
+        //    await using (FileStream FileStream = System.IO.File.Create
+        //        (_environment.WebRootPath + "\\Upload\\" + objFile.File.FileName))
+        //    {
+        //        objFile.File.CopyTo(FileStream);
+        //        FileStream.Flush();// Очищает буферы для этого потока и вызывает запись всех буферизованных данных в файл
+        //        return "added file " + objFile.File.FileName;
+        //    }
+        //}
+
+        /// <summary>
+        /// Choose image from directory and upload it on server
+        /// </summary>
+        /// <param name="file">file that was selected for upload</param>
+        /// <returns></returns>
+        [HttpPost]       
         public async Task<IActionResult> Upload([FromForm] IFormFile file)
         {
             if (file != null)
@@ -77,10 +84,7 @@ namespace UploadDownloadFile_WebAPI_Sample.Controllers
                 }
             }
             else
-            {
                 return new StatusCodeResult(204);
-            }
         }
     }
-
 }
